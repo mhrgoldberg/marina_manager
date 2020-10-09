6ea10a476e1a968c56f366d858906db8de4fb7b1
mitchellreiss <mitchellreiss@gmail.com>
2020-10-09 08:52:14 -0700

    Update seed data to for 20 spots

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,4 @@
 
 Spot.destroy_all
 
-Spot.create([{ spot_number: 1 }, { spot_number: 2 }, { spot_number: 3 }, { spot_number: 4 }, { spot_number: 5 }, { spot_number: 6 }, { spot_number: 7 }, { spot_number: 8 }, { spot_number: 9 }, { spot_number: 10 }])
+Spot.create([{ spot_number: 1 }, { spot_number: 2 }, { spot_number: 3 }, { spot_number: 4 }, { spot_number: 5 }, { spot_number: 6 }, { spot_number: 7 }, { spot_number: 8 }, { spot_number: 9 }, { spot_number: 10 }, { spot_number: 11 }, { spot_number: 12 }, { spot_number: 13 }, { spot_number: 14 }, { spot_number: 15 }, { spot_number: 16 }, { spot_number: 17 }, { spot_number: 18 }, { spot_number: 19 }, { spot_number: 20 }])
